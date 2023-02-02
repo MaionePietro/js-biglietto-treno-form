36c8cb52af7485e32d32e6cdfdf930ec8dde4f95
MaionePietro <pietromaionespam@gmail.com>
2023-02-02 12:04:56 +0100

    FEAT swirch case

--- js/app.js
+++ js/app.js
@@ -1,34 +1,73 @@
-// 0: Chiedere nome e cognome 
 
-let nome, cognome;
-console.log(nome, cognome);
+// // 3: Calcolare il prezzo totale dei kilometri
 
-// 1: Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
+// console.log(kmPrezzo);
 
-km = parseInt(km);
-console.log(km);
 
-// 2: Chiedere l'età del passeggero
-
-anni = parseInt(anni);
-console.log(anni);
-
-// 3: Calcolare il prezzo totale dei kilometri
-
-const kmPrezzo = (0.21 );
-console.log(kmPrezzo);
-
-const totKm = (km * kmPrezzo);
-console.log('prezzo chilometri: ' + totKm + ' €');
-
-// 4: applicare sconto in base all'età 20% junior e 40% senior
-
+const btcElement = document.querySelector('button');
 let sconto =0, prezzoFinale, prezzoDecimali;
-if(anni <= 18){
-    sconto = (totKm / 100) * 20;
-}else if(anni >= 65){
-    sconto = (totKm / 100) * 40;
-}
-prezzoFinale = totKm - sconto;
-prezzoDecimali = prezzoFinale.toFixed(2);
-console.log('prezzo finale: ' + prezzoDecimali + '€');
+const kmPrezzo = (0.21 );
+
+btcElement.addEventListener('click', function(){
+    //nome cognome
+    const nomeCognome = document.querySelector('input[type="nome"]')
+    console.log(nomeCognome.value);
+
+    //km precorrere
+    const kmPercorrere = document.querySelector('input[type="kmPercorsi"]')
+    console.log(kmPercorrere.value);
+    let km = parseInt(kmPercorrere.value);
+    const totKm = (km * kmPrezzo);
+    console.log('prezzo chilometri: ' + totKm + ' €');
+
+    //fascia età
+    const fasciaEta = document.querySelector('select[name="eta"]')
+    console.log(fasciaEta.value);
+    let color = fasciaEta.value;
+
+    switch(color) {
+        case '1':
+            sconto = (totKm / 100) * 20;
+            prezzoFinale = totKm - sconto;
+            prezzoDecimali = prezzoFinale.toFixed(2);
+            break
+        case '2':
+            sconto = (totKm / 100) * 40;
+            prezzoFinale = totKm - sconto;
+            prezzoDecimali = prezzoFinale.toFixed(2);
+            break
+        default:
+            console.log
+            prezzoFinale = totKm
+            prezzoDecimali = prezzoFinale.toFixed(2);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 4: applicare sconto in base all'età 20% junior e 40% senior
+
+// let sconto =0, prezzoFinale, prezzoDecimali;
+// if(anni <= 18){
+//     sconto = (totKm / 100) * 20;
+// }else if(anni >= 65){
+//     sconto = (totKm / 100) * 40;
+// }
+// prezzoFinale = totKm - sconto;
+// prezzoDecimali = prezzoFinale.toFixed(2);
+// console.log('prezzo finale: ' + prezzoDecimali + '€');
